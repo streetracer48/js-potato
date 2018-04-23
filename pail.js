5ef9a9cfab1494c6928304cc8b3c0c62a656e68b
wasifmiraz <wasifmiraz@mail.com>
2018-04-23 14:15:41 +0600

    this is simple js function work

---
+++ pail.js
@@ -0,0 +1,31 @@
+/**
+ * Write a function on_all that applies a function to every element of a list.
+ * You cannot use Array.map, but have to implement it yourself.
+ */
+var MyFunction = function() {
+  'use strict';
+
+  function applyFunction(list, f) {
+
+  }
+
+  return {
+    applyFunction: applyFunction
+  };
+};
+
+/**
+ * Write a function that outputs a multiplication table for numbers up to 5.
+ * The table should be a two-dimensional array.
+ */
+var MultiplicationTable = function() {
+    'use strict';
+  
+    function generate5() {
+  
+    }
+  
+    return {
+      generate5: generate5
+    };
+  };
